61e3608792dd4e08c7a628c3a65f5526d6691c1b
otake-kensuke <otake.kensuke@gmail.com>
2026-09-13 15:54:30 +0900

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -5,8 +5,8 @@ const EMBASSY_MASTER = [
     embassyName: "ペルー大使館",
     day: 1,
     order: 1,
-    address: "東京都渋谷区広尾",
-    googleMapsQuery: "ペルー大使館 東京都"
+    address: "〒150-0012 東京都渋谷区広尾2-3-1",
+    googleMapsQuery: "ペルー大使館 〒150-0012 東京都渋谷区広尾2-3-1"
   },
   {
     id: "czech",
@@ -14,8 +14,8 @@ const EMBASSY_MASTER = [
     embassyName: "チェコ大使館",
     day: 1,
     order: 2,
-    address: "東京都渋谷区広尾",
-    googleMapsQuery: "チェコ大使館 東京都"
+    address: "〒150-0012 東京都渋谷区広尾2-16-14",
+    googleMapsQuery: "チェコ大使館 〒150-0012 東京都渋谷区広尾2-16-14"
   },
   {
     id: "croatia",
@@ -23,8 +23,8 @@ const EMBASSY_MASTER = [
     embassyName: "クロアチア大使館",
     day: 1,
     order: 3,
-    address: "東京都渋谷区広尾",
-    googleMapsQuery: "クロアチア大使館 東京都"
+    address: "〒150-0012 東京都渋谷区広尾3-3-10",
+    googleMapsQuery: "クロアチア大使館 〒150-0012 東京都渋谷区広尾3-3-10"
   },
   {
     id: "oman",
@@ -32,8 +32,8 @@ const EMBASSY_MASTER = [
     embassyName: "オマーン大使館",
     day: 1,
     order: 4,
-    address: "東京都渋谷区広尾",
-    googleMapsQuery: "オマーン大使館 東京都"
+    address: "〒150-0012 東京都渋谷区広尾4-2-17",
+    googleMapsQuery: "オマーン大使館 〒150-0012 東京都渋谷区広尾4-2-17"
   },
   {
     id: "switzerland",
@@ -41,7 +41,79 @@ const EMBASSY_MASTER = [
     embassyName: "スイス大使館",
     day: 1,
     order: 5,
-    address: "東京都港区南麻布",
-    googleMapsQuery: "スイス大使館 東京都"
+    address: "〒106-8589 東京都港区南麻布5-9-12",
+    googleMapsQuery: "スイス大使館 〒106-8589 東京都港区南麻布5-9-12"
+  },
+  {
+    id: "bosnia-herzegovina",
+    country: "ボスニア・ヘルツェゴビナ",
+    embassyName: "ボスニア・ヘルツェゴビナ大使館",
+    day: 1,
+    order: 6,
+    address: "〒106-0047 東京都港区南麻布5-3-29",
+    googleMapsQuery: "ボスニア・ヘルツェゴビナ大使館 〒106-0047 東京都港区南麻布5-3-29"
+  },
+  {
+    id: "france",
+    country: "フランス",
+    embassyName: "フランス大使館",
+    day: 1,
+    order: 7,
+    address: "〒106-8514 東京都港区南麻布4-11-44",
+    googleMapsQuery: "フランス大使館 〒106-8514 東京都港区南麻布4-11-44"
+  },
+  {
+    id: "pakistan",
+    country: "パキスタン",
+    embassyName: "パキスタン大使館",
+    day: 1,
+    order: 8,
+    address: "〒106-0047 東京都港区南麻布4-6-17",
+    googleMapsQuery: "パキスタン大使館 〒106-0047 東京都港区南麻布4-6-17"
+  },
+  {
+    id: "cyprus",
+    country: "キプロス",
+    embassyName: "キプロス大使館",
+    day: 1,
+    order: 9,
+    address: "〒106-0047 東京都港区南麻布4-6-28",
+    googleMapsQuery: "キプロス大使館 〒106-0047 東京都港区南麻布4-6-28"
+  },
+  {
+    id: "germany",
+    country: "ドイツ",
+    embassyName: "ドイツ大使館",
+    day: 1,
+    order: 10,
+    address: "〒106-0047 東京都港区南麻布4-5-10",
+    googleMapsQuery: "ドイツ大使館 〒106-0047 東京都港区南麻布4-5-10"
+  },
+  {
+    id: "finland",
+    country: "フィンランド",
+    embassyName: "フィンランド大使館",
+    day: 1,
+    order: 11,
+    address: "〒106-8561 東京都港区南麻布3-5-39",
+    googleMapsQuery: "フィンランド大使館 〒106-8561 東京都港区南麻布3-5-39"
+  },
+  {
+    id: "iran",
+    country: "イラン",
+    embassyName: "イラン大使館",
+    day: 1,
+    order: 12,
+    address: "〒106-0047 東京都港区南麻布3-13-9",
+    googleMapsQuery: "イラン大使館 〒106-0047 東京都港区南麻布3-13-9"
+  },
+  {
+    id: "korea",
+    country: "韓国",
+    embassyName: "韓国大使館",
+    day: 1,
+    order: 13,
+    address: "〒106-0047 東京都港区南麻布1-2-5",
+    googleMapsQuery: "韓国大使館 〒106-0047 東京都港区南麻布1-2-5"
   }
 ];
